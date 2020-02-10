5d68796abee2e8ef234ffcb8a86407d3786a3b38
hieplq <hieplq@hasuvimex.vn>
2020-02-10 10:35:06 +0700

    fix dependency enough for call service. remain config security

--- co.globalqss.fedian.imp/src/main/java/co/globalqss/fedian/imp/TestCallService.java
+++ co.globalqss.fedian.imp/src/main/java/co/globalqss/fedian/imp/TestCallService.java
@@ -9,6 +9,9 @@ import javax.xml.ws.WebServiceFeature;
 import javax.xml.ws.soap.AddressingFeature;
 
 import org.apache.cxf.jaxws.spi.ProviderImpl;
+import org.apache.cxf.ws.addressing.AddressingProperties;
+import org.apache.cxf.ws.addressing.AttributedURIType;
+import org.apache.cxf.ws.addressing.EndpointReferenceType;
 import org.apache.felix.service.command.annotations.GogoCommand;
 import org.datacontract.schemas._2004._07.dianresponse.DianResponse;
 import org.osgi.service.component.annotations.Component;
@@ -51,46 +54,60 @@ public class TestCallService implements CallService{
 			// factory.setFeatures(...);  // if required
 			WebServiceFeature serviceCxf = factory.create(IWcfDianCustomerServices.class);        
 			Client client = ClientProxy.getClient(winrm);*/
-			
-
-			/*LCO_FE_HeaderHandlerResolverCert handlerResolver = new LCO_FE_HeaderHandlerResolverCert();
-			String cerPass = System.getProperty ("cerPass");
-			String cerFilePath = System.getProperty ("cerFilePath");
-			handlerResolver.setParameters(cerFilePath, cerPass);
-			service.setHandlerResolver(handlerResolver);*/
 	
 			WebServiceFeature address = new AddressingFeature(true, true, AddressingFeature.Responses.ANONYMOUS);
 			port = service.getWSHttpBindingIWcfDianCustomerServices(address);
-			
-			//https://docs.oracle.com/cd/E17266_01/English/Technical_Documentation/Web_Services/ProgrammersGuide/helpmain.htm?toc.htm?34314.htm
-			//https://github.com/eclipse-ee4j/jaxrs-api/issues/467
-			//https://stackoverflow.com/a/36009031
-			//https://stackoverflow.com/a/38732953
-			/*
-			Client client = ClientProxy.getClient(port);
-			HTTPConduit httpConduit = (HTTPConduit)client.getConduit();
-			HTTPClientPolicy policy = httpConduit.getClient();
-			policy.setReceiveTimeout(0);
-			policy.setConnectionRequestTimeout()
-			policy.setConnectionTimeout();
-			*/
 
 
 			Map<String, Object> ctx = ((BindingProvider) port).getRequestContext();
-			//ctx.put(BindingProviderProperties.CONNECT_TIMEOUT, 10000);
-			//ctx.put(BindingProviderProperties.REQUEST_TIMEOUT, 10000);
-			//ctx.put(BindingProviderProperties.ADDRESSING_TO, "https://vpfe-hab.dian.gov.co/WcfDianCustomerServices.svcx");
-			ctx.put("org.apache.cxf.ws.addressing.replyto", "https://vpfe-hab.dian.gov.co/WcfDianCustomerServices.svcx");
-			/* more flexible way
-			AddressingProperties maps = new AddressingPropertiesImpl();
-			EndpointReferenceType ref = new EndpointReferenceType();
-			AttributedURIType add = new AttributedURIType();
-			add.setValue("http://localhost:9090/decoupled_endpoint");
-			ref.setAddress(add);
-			maps.setReplyTo(ref);
-			maps.setFaultTo(ref);
-			((BindingProvider)port).getRequestContext().put("javax.xml.ws.addressing.context", maps);
+			/************
+			 * Timeout
+			 * 1. see sun.net.NetworkClient. time out can be setting by system properties "sun.net.client.defaultReadTimeout" and "sun.net.client.defaultConnectTimeout"
+			 * 2. for cfx can be override <1> by bellow code: from https://stackoverflow.com/a/38732953
+			    Client client = ClientProxy.getClient(port);
+				HTTPConduit httpConduit = (HTTPConduit)client.getConduit();
+				HTTPClientPolicy policy = httpConduit.getClient();
+				policy.setReceiveTimeout(0);
+				policy.setConnectionRequestTimeout()
+				policy.setConnectionTimeout();
+			 * 3. for metro use context properties
+			    ctx.put(BindingProviderProperties.CONNECT_TIMEOUT, 10000);
+			    ctx.put(BindingProviderProperties.REQUEST_TIMEOUT, 10000);
 			 */
+			
+			/********
+			 * Setting Addressing Feature: https://cxf.apache.org/docs/ws-addressing.html
+			 * 1. cxf
+			    ctx.put("org.apache.cxf.ws.addressing.replyto", "https://vpfe-hab.dian.gov.co/WcfDianCustomerServices.svcx");
+			 * more attribution also more flexible way for cxf
+				AddressingProperties maps = new AddressingPropertiesImpl();
+				EndpointReferenceType ref = new EndpointReferenceType();
+				AttributedURIType add = new AttributedURIType();
+				add.setValue("http://localhost:9090/decoupled_endpoint");
+				ref.setAddress(add);
+				maps.setReplyTo(ref);
+				maps.setFaultTo(ref);
+				((BindingProvider)port).getRequestContext().put("javax.xml.ws.addressing.context", maps);
+			 */
+			AddressingProperties maps = new AddressingProperties();
+			EndpointReferenceType ref = new EndpointReferenceType();
+			AttributedURIType action = new AttributedURIType();
+			action.setValue("http://wcf.dian.colombia/IWcfDianCustomerServices/GetStatus");
+			maps.setAction(action);
+			
+			// on SoapUI this value is "add default wsa:to"
+			AttributedURIType to = new AttributedURIType();
+			to.setValue("https://vpfe-hab.dian.gov.co/WcfDianCustomerServices.svc");
+			maps.setTo(to);
+			
+			maps.setRequired(true);
+
+			ctx.put("javax.xml.ws.addressing.context", maps);
+			
+			// TODO:at moment ever has org.apache.felix.http.api, HTTPTransportActivator don wire to package org.osgi.service.http.HttpService, so it can't load
+			// temp disable http.transport for test, fix dependency later
+			System.setProperty("org.apache.cxf.osgi.http.transport.disable", "true");
+			
 			DianResponse status = port.getStatus("bc84a90ee7ae3bcd05961195cb5391dbce7271016978f70648c530ef9b6f87b723fce42167c774b958382d3423d28fc2");
 			
 			return "ok";
